55526a23e9d18e6c11b19ea7b4d80ade0cd1c5fd
Johannes <johannesr@viken.no>
2022-08-07 19:17:54 +0200

    fixed some bugs with backup

--- python/main.py
+++ python/main.py
@@ -17,7 +17,7 @@ def format_time_to_md(time):
         day = "0"+day
     return day + "-" + month + "-"+str(time.year)+".md"
 
-def backup_file_with_scp(filename: string, destination: string):
+def backup_file_with_scp(filename: str, destination: str):
     try:
         subprocess.call(["scp", filename, destination])
     except:
@@ -29,13 +29,14 @@ def move_file(src, target):
     with open(src+'note.md', 'r') as f:
         line = f.readline()
         if line != DEFAULT_TEXT:
-            file_name = format_time_to_md(now) 
+            file_name = format_time_to_md(now)
             shutil.move(src + 'note.md', target + file_name)
             print("moved: "+src + "note.md " + "to: " + target + file_name)
-            backup_file_with_scp(file_name, DEFAULT_DEST)
+            backup_file_with_scp(target+file_name, DEFAULT_DEST)
         else:
+            print("It appears you havent modified the file yet, make sure to save your changes before calling this!")
             return 1
-        
+
 if __name__ == "__main__":
     #create_note_md(DESKTOP_DIR)
     #move_file(DESKTOP_DIR, TARGET_DIR)
